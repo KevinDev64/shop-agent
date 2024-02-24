5a8ce29292f66f7eaf7a1600b8e2c194a52fb856
KevinDev64 <kevindev56@yandex.ru>
2024-02-24 12:04:58 +0300

    feat: split and translate to vectors texts for bot
    This code divides the text into chunks, converts the data into numeric vectors, and stores them in the database. This is necessary for the further operation of the Telegram bot.

---
+++ utils/teach.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import os
+import langchain
+from pprint import pprint as pp
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+import warnings
+
+token_file = open("token.txt", "r")
+token = token_file.readline()
+token_file.close()
+
+os.environ["OPENAI_API_KEY"] = token
+
+number_of_goods = 6 # fix this
+
+print("-------------------------------- splitting")
+data_plus = []
+path = '../input/'
+text_loader_kwargs={'autodetect_encoding': True}
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=10,
+    separators=["\n\n", "\n", "(?<=\. )"]
+)
+for i in range(number_of_goods):
+    loader = DirectoryLoader(path, glob="text"+str(i)+".txt", loader_cls=TextLoader,
+                             loader_kwargs=text_loader_kwargs)
+    docs = loader.load()
+    
+    data_plus.append(splitter.split_documents(docs))
+    
+pp(data_plus)
+input("Waiting for Enter to continue...")
+print("-------------------------------- translate to vectors & save to files...")
+directory = ''
+embedding = OpenAIEmbeddings()
+vectordb_list = []
+for i in range(number_of_goods):
+    vectordb_list.append(Chroma.from_documents(documents=data_plus[i], 
+                                               embedding=embedding, persist_directory="../output/"+str(i)))
+for j in range(number_of_goods):
+    vectordb_list[j].persist()
+    print(vectordb_list[j]._collection.count())
