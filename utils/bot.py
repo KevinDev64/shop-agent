f06967cacc2d30f6fdafb6832c412bbede70b59a
KevinDev64 <kevindev56@yandex.ru>
2024-02-27 18:00:16 +0300

    fix: add missing quotes

--- utils/bot.py
+++ utils/bot.py
@@ -22,7 +22,7 @@ cur = conn.cursor()
 logging.basicConfig(level=logging.INFO, filename="../info.log", filemode='w')
 
 # init a bot with token from file
-bot_token_file = open(bot_token.txt, 'r')
+bot_token_file = open("bot_token.txt", "r")
 bot_token = bot_token_file.readline()
 bot_token_file.close()
 bot = telebot.TeleBot(bot_token)
